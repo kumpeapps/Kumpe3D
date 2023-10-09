3721ce4437252e64e8eeecfb2ed8de084757f986
Justin Kumpe <jakumpe@kumpes.com>
2023-10-08 20:08:29 -0500

    -

--- shop-checkout.php
+++ shop-checkout.php
@@ -50,7 +50,7 @@ mysqli_close($conn);
 <html lang="en">
 
 <head>
-	<script type="text/javascript" src="https://app.termly.io/embed.min.js" data-auto-block="on"
+	<script type="text/javascript" src="https://app.termly.io/embed.min.js" data-auto-block="off"
 		data-website-uuid="f0526f09-9728-4a75-853d-72961022b400"></script>
 	<!-- Meta -->
 	<meta charset="utf-8">
