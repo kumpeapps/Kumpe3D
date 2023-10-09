c943966c998e694b9569e41420da2291f9cd0417
Justin Kumpe <jakumpe@kumpes.com>
2023-10-08 21:51:17 -0500

    -

--- header.js
+++ header.js
@@ -113,3 +113,11 @@ function cartQtyChange(event) {
         deleteItem(sku);
     }
 };
+
+if ($env == "dev") {
+    Swal.fire(
+        'PreProd Server',
+        'This is a PreProd/Development server. Orders submitted via this site will not be charged or filled.',
+        'information'
+    );
+}
