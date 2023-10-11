ac6fb5f3a66f4f6478739b52e1bfee265c5cc81f
Justin Kumpe <jakumpe@kumpes.com>
2023-10-10 19:20:54 -0500

    -

--- product.php
+++ product.php
@@ -74,7 +74,7 @@ $filaments_sql = "CALL get_filament_options('$base_sku', '$filament_filter');";
 <head>
 	<!-- Meta -->
 	<meta http-equiv="Content-Security-Policy" content="default-src 'self';
-script-src 'report-sample' 'self' https://cdn.jsdelivr.net/npm/sweetalert2@11 https://unpkg.com/cart-localstorage@1.1.4/dist/cart-localstorage.min.js 'nonce-<?php echo $nonce; ?>';
+script-src https://cdn.jsdelivr.net/npm/sweetalert2@11 https://unpkg.com/cart-localstorage@1.1.4/dist/cart-localstorage.min.js 'nonce-<?php echo $nonce; ?>';
 style-src 'report-sample' 'self' https://fonts.googleapis.com 'nonce-<?php echo $nonce; ?>';
 object-src 'nonce-<?php echo $nonce; ?>';
 base-uri 'self' 'nonce-<?php echo $nonce; ?>';
