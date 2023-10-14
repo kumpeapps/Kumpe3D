c355f452fe47cefaab6ba62a6c56b816e74e01f8
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 13:33:48 -0500

    -

--- http-methods.js
+++ http-methods.js
@@ -1,6 +1,11 @@
-function Get(yourUrl){
+function Get(yourUrl, return_json = true) {
     var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",yourUrl,false);
+    Httpreq.open("GET", yourUrl, false);
     Httpreq.send(null);
-    return Httpreq.responseText;          
+    response = Httpreq.responseText;
+    if (return_json) {
+        return JSON.parse(response);
+    } else {
+        return response;
+    }
 }
