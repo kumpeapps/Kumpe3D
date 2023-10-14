85b2dedb1989a68e172a9f1f075e21cc61cc52fe
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 14:31:59 -0500

    -

--- product.php
+++ product.php
@@ -1,4 +1,14 @@
 <?php
+// **PREVENTING SESSION HIJACKING**
+// Prevents javascript XSS attacks aimed to steal the session ID
+ini_set('session.cookie_httponly', 1);
+
+// **PREVENTING SESSION FIXATION**
+// Session ID cannot be passed through URLs
+ini_set('session.use_only_cookies', 1);
+
+// Uses a secure connection (HTTPS) if possible
+ini_set('session.cookie_secure', 1);
 session_start();
 require_once 'includes/site_params.php';
 $conn = mysqli_connect(
