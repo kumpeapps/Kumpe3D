89a027940d9aad45175c71d00ec3f23d8255225e
Justin Kumpe <jakumpe@kumpes.com>
2023-10-16 21:09:06 -0500

    -

--- product.php
+++ product.php
@@ -82,8 +82,7 @@ $filaments_sql = "CALL get_filament_options('$base_sku', '$filament_filter');";
 	<meta default-src 'self' ; script-src 'self'; style-src 'self'
 		https://fonts.googleapis.com; object-src 'none' ; base-uri 'self' ; connect-src 'self'
 		https://api.preprod.kumpe3d.com; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' ; img-src '*'
-		https://images.kumpeapps.com; manifest-src 'self' ; media-src 'self' ; worker-src 'none' ;
-		Content-Security-Policy-Report-Only ;>
+		https://images.kumpeapps.com; manifest-src 'self' ; media-src 'self' ; worker-src 'none' ;>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="keywords" content="<?php echo $product['tags']; ?>">
