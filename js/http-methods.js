521f818099dd6287af43de8c67b0cb39f95f487d
Justin Kumpe <jakumpe@kumpes.com>
2023-10-19 21:13:49 -0500

    -

--- js/http-methods.js
+++ js/http-methods.js
@@ -22,19 +22,6 @@ function POST(yourUrl, data, return_json = true) {
     }
 };
 
-function postJSON(yourUrl, data, return_json = true) {
-    var Httpreq = new XMLHttpRequest(); // a new request
-    JSON.stringify(data);
-    Httpreq.open("POST", yourUrl, false);
-    Httpreq.send(data);
-    response = Httpreq.responseText;
-    if (return_json) {
-        return JSON.parse(response);
-    } else {
-        return response;
-    }
-};
-
 const getJSON = async url => {
     const response = await fetch(url);
     if (!response.ok) // check if response worked (no 404 errors etc...)
@@ -45,3 +32,21 @@ const getJSON = async url => {
     else
         return response.json();
 };
+
+function postJSON(yourUrl, data){
+    // Creating a XHR object
+    let xhr = new XMLHttpRequest();
+    let url = yourUrl;
+
+    // open a connection
+    xhr.open("POST", url, true);
+
+    // Set the request header i.e. which type of content you are sending
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    // Converting JSON data to string
+    var data = JSON.stringify(data);
+
+    // Sending data with the request
+    xhr.send(data);
+}
