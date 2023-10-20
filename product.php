f4424df0de2ee925940ed55014b197ebd5f4d2eb
Justin Kumpe <jakumpe@kumpes.com>
2023-10-19 19:58:32 -0500

    -

--- product.php
+++ product.php
@@ -380,11 +380,11 @@ $filaments_sql = "CALL get_filament_options('$base_sku', '$filament_filter');";
 		<!-- CUSTOM JS -->
 		<script nonce="<?php echo $nonce; ?>" src="js/product.js"></script>
 
-		<script nonce="<?php echo $nonce; ?>">
+		<!-- <script nonce="">
 
 			function addToCart() {
 				const sku = skuLabel.innerHTML;
-				const base_sku = '<?php echo $base_sku; ?>';
+				const base_sku = '';
 				const color_id = getColorValue();
 				const image_url_base = 'https://images.kumpeapps.com/filament_swatch?sku=';
 				const image_url = image_url_base + base_sku + '-' + color_id;
@@ -406,7 +406,7 @@ $filaments_sql = "CALL get_filament_options('$base_sku', '$filament_filter');";
 						{
 							id: sku,
 							sku: sku,
-							name: "<?php echo $product['title']; ?> (" + color_id + ")",
+							name: " (" + color_id + ")",
 							price: itemPrice,
 							image_url: image_url,
 							original_price: originalPrice,
@@ -421,7 +421,7 @@ $filaments_sql = "CALL get_filament_options('$base_sku', '$filament_filter');";
 				updateShoppingCartModal();
 			};
 
-		</script>
+		</script> -->
 
 </body>
 
