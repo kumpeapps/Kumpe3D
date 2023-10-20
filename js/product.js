ec2e935d0b2a02a875f96043c9637fb9474f9c7e
Justin Kumpe <jakumpe@kumpes.com>
2023-10-20 10:34:22 -0500

    -

--- js/product.js
+++ js/product.js
@@ -58,7 +58,7 @@ function buildColorOptions() {
     colorOptions.forEach(build);
     function build(element, _, _) {
         const div = document.createElement("div");
-        div.setAttribute("class", "radio-value image-radio border border-secondary rounded");
+        div.setAttribute("class", "radio-value image-radio border border-success rounded");
         const input = document.createElement("input");
         input.setAttribute("class", "form-check-input radio-value");
         input.setAttribute("type", "radio");
