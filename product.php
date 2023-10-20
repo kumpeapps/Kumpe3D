eb0775d098d19e01ee37dc14790bcba15e733292
Justin Kumpe <jakumpe@kumpes.com>
2023-10-20 11:20:23 -0500

    -

--- product.php
+++ product.php
@@ -119,8 +119,7 @@ $filaments_sql = "CALL get_filament_options('$base_sku', '$filament_filter');";
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- STYLESHEETS -->
-	<link nonce="<?php echo $nonce; ?>" rel="stylesheet" type="text/css"
-		href="vendor-js/bootstrap-select/dist/css/bootstrap-select.min.css">
+	<link nonce="<?php echo $nonce; ?>" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
 	<link nonce="<?php echo $nonce; ?>" rel="stylesheet" type="text/css" href="icons/fontawesome/css/all.min.css">
 	<link nonce="<?php echo $nonce; ?>" rel="stylesheet" type="text/css" href="icons/themify/themify-icons.css">
 	<link nonce="<?php echo $nonce; ?>" rel="stylesheet" type="text/css" href="icons/flaticon/flaticon_mooncart.css">
