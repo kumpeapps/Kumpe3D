4c521c30fb051efafbfdec30e61a939cf1dd317d
Justin Kumpe <jakumpe@kumpes.com>
2023-10-20 13:20:27 -0500

    -

--- shop-checkout.php
+++ shop-checkout.php
@@ -324,9 +324,9 @@ require_once 'includes/site_params.php';
 										<tr class="taxes">
 											<!-- Start Taxes -->
 											<td id="stateTax">
-												Arkansas: $5
-												Rogers: $5
-												Benton County: $2
+												Arkansas: $5<br>
+												Rogers: $5<br>
+												Benton County: $2<br>
 											</td>
 											<td id="totalTax" class="price">
 												Total Tax $12
