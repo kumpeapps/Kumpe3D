e695a85574987f3219c249de7bf23c7cac09193b
Justin Kumpe <jakumpe@kumpes.com>
2023-10-20 13:14:05 -0500

    -

--- shop-checkout.php
+++ shop-checkout.php
@@ -321,6 +321,21 @@ require_once 'includes/site_params.php';
 												Total Tax $0
 											</td>
 										</tr>
+										<tr class="taxes">
+											<!-- Start Taxes -->
+											<td id="stateTax">
+												Arkansas: $5
+											</td>
+											<td id="cityTax">
+												Rogers: $5
+											</td>
+											<td id="countyTax">
+												Benton County: $2
+											</td>
+											<td id="totalTax" class="price">
+												$12
+											</td>
+										</tr>
 										<tr class="total">
 											<td>Total</td>
 											<td id="cart_total" class="price">$0</td>
