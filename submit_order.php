1ab2e32804e904633fd0f1c0e4f0b8fe65742fec
Justin Kumpe <jakumpe@kumpes.com>
2023-10-21 14:34:37 -0500

    -

--- submit_order.php
+++ submit_order.php
@@ -94,7 +94,7 @@ if (1==1) {
     $email_notes = $data['orderNotes'];
     $stmt = $db->prepare($sql);
     $stmt->bind_param(
-        "issssssssssdddddisss",
+        "issssssssssdddddisssssssss",
         $data['customerID'],
         $data['firstName'],
         $data['lastName'],
