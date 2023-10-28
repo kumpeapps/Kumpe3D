cb05384d5277d9e99c494ea2cc22ff5b2d568ea2
Justin Kumpe <jakumpe@kumpes.com>
2023-10-27 20:58:50 -0500

    -

--- includes/footer.php
+++ includes/footer.php
@@ -71,34 +71,4 @@ $conn = mysqli_connect(
 	<a href="#" onclick="window.displayPreferenceModal();return false;" id="termly-consent-preferences">Consent
 		Preferences</a>
 </footer>
-<!-- Messenger Chat Plugin Code -->
-<div id="fb-root"></div>
-
-<!-- Your Chat Plugin code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-
-<script nonce="<?php echo $nonce; ?>">
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute("page_id", "156967714165483");
-  chatbox.setAttribute("attribution", "biz_inbox");
-</script>
-
-<!-- Your SDK code -->
-<script nonce="<?php echo $nonce; ?>">
-  window.fbAsyncInit = function() {
-	FB.init({
-	  xfbml            : true,
-	  version          : 'v18.0'
-	});
-  };
-
-  (function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-	fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
 <!-- Footer End -->
