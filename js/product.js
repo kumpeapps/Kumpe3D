21ac813b46cb23c96f8cc33c660c00cb6f1f8558
Justin Kumpe <jakumpe@kumpes.com>
2023-10-31 20:41:47 -0500

    -

--- js/product.js
+++ js/product.js
@@ -209,10 +209,10 @@ function addToCart(element) {
     console.debug("after modal");
     const button = document.createElement("a");
     button.setAttribute("class", "btn btn-secondary w-100");
-    button.setAttribute("id", "addToCartButton");
+    button.setAttribute("id", "addToCartButton2-");
     button.innerHTML = "ADD TO CART";
     document.getElementById("addToCartContainer").appendChild(button);
-    const addToCartButton = document.querySelector("#addToCartButton");
+    const addToCartButton = document.querySelector("#addToCartButton2");
     addToCartButton.addEventListener("click", function () {
         addToCart();
     }, once= true);
