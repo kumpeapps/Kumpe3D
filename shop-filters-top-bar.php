95fb00b195b7d573f6865f5f593988b8b56e13c0
Justin Kumpe <jakumpe@kumpes.com>
2023-11-04 21:37:24 -0500

    -

--- shop-filters-top-bar.php
+++ shop-filters-top-bar.php
@@ -105,11 +105,10 @@ $conn = mysqli_connect(
 		<div class="dz-bnr-inr style-1" style="background-image:url(images/background/bg-shape.jpg);">
 			<div class="container">
 				<div class="dz-bnr-inr-entry">
-					<h1>Shop Top Filters</h1>
+					<h1>Shop Kumpe3D</h1>
 					<nav aria-label="breadcrumb" class="breadcrumb-row">
 						<ul class="breadcrumb">
 							<li class="breadcrumb-item"><a href="index.html"> Home</a></li>
-							<li class="breadcrumb-item">Shop Standard</li>
 						</ul>
 					</nav>
 				</div>
@@ -141,7 +140,7 @@ $conn = mysqli_connect(
 								</ul>
 								<span>Showing 1–5 Of 50 Results</span>
 							</div>
-							<div class="filter-right-area">
+							<!-- <div class="filter-right-area">
 								<div class="form-group">
 									<a href="javascript:void(0);" class="filter-top-btn" id="filterTopBtn">
 										<svg class="me-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="20" height="20"><g id="Layer_29" data-name="Layer 29"><path d="M2.54,5H15v.5A1.5,1.5,0,0,0,16.5,7h2A1.5,1.5,0,0,0,20,5.5V5h2.33a.5.5,0,0,0,0-1H20V3.5A1.5,1.5,0,0,0,18.5,2h-2A1.5,1.5,0,0,0,15,3.5V4H2.54a.5.5,0,0,0,0,1ZM16,3.5a.5.5,0,0,1,.5-.5h2a.5.5,0,0,1,.5.5v2a.5.5,0,0,1-.5.5h-2a.5.5,0,0,1-.5-.5Z"></path><path d="M22.4,20H18v-.5A1.5,1.5,0,0,0,16.5,18h-2A1.5,1.5,0,0,0,13,19.5V20H2.55a.5.5,0,0,0,0,1H13v.5A1.5,1.5,0,0,0,14.5,23h2A1.5,1.5,0,0,0,18,21.5V21h4.4a.5.5,0,0,0,0-1ZM17,21.5a.5.5,0,0,1-.5.5h-2a.5.5,0,0,1-.5-.5v-2a.5.5,0,0,1,.5-.5h2a.5.5,0,0,1,.5.5Z"></path><path d="M8.5,15h2A1.5,1.5,0,0,0,12,13.5V13H22.45a.5.5,0,1,0,0-1H12v-.5A1.5,1.5,0,0,0,10.5,10h-2A1.5,1.5,0,0,0,7,11.5V12H2.6a.5.5,0,1,0,0,1H7v.5A1.5,1.5,0,0,0,8.5,15ZM8,11.5a.5.5,0,0,1,.5-.5h2a.5.5,0,0,1,.5.5v2a.5.5,0,0,1-.5.5h-2a.5.5,0,0,1-.5-.5Z"></path></g></svg>
@@ -192,7 +191,7 @@ $conn = mysqli_connect(
 										</li>
 									</ul>
 								</div>
-							</div>
+							</div> -->
 						</div>
 						<div class="col-xl-12 shop-top-filter">
 							<a href="javascript:void(0);" class="panel-close-btn">																	
