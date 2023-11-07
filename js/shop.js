612bab2e77a8477ee00b21e6daa42eac9082663a
Justin Kumpe <jakumpe@kumpes.com>
2023-11-06 19:24:14 -0600

    -

--- js/shop.js
+++ js/shop.js
@@ -5,8 +5,8 @@ function getProducts(sku = "%", category = "%", tag = "%") {
     topCount = document.getElementById("resultsCountTop");
     bottomCount = document.getElementById("resultsCountBottom");
     products = GET(apiUrl + "/product?sku=" + sku + "&category_filter=" + category + "&tag_filter=" + tag + "&search=%").response;
-    topCount.innerHTML = products.length();
-    bottomCount.innerHTML = products.length();
+    topCount.innerHTML = products.length;
+    bottomCount.innerHTML = products.length;
 };
 
 function load() {
