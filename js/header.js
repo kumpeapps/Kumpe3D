1727c249759dbc7dfcf47616981ea192fceb800d
Justin Kumpe <jakumpe@kumpes.com>
2023-11-07 20:24:44 -0600

    -

--- js/header.js
+++ js/header.js
@@ -25,7 +25,7 @@ function buildShoppingCartModalList() {
     function renderShoppingCartModalList(element, _, _) {
         const img_url = element["img_url"];
         let title = element["productTitle"] + "<br>(" + element['colorTitle'] + ")";
-        if (element['colorTitle'] == null) {
+        if (element['colorTitle'] == "null") {
             title = element["productTitle"];
         }
         const qty = element["quantity"];
