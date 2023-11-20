b0c38af1062b7077f47ee4bb3208fd4cf02a35c4
Justin Kumpe <jakumpe@kumpes.com>
2023-11-19 19:46:25 -0600

    -

--- index.php
+++ index.php
@@ -1693,23 +1693,23 @@ $conn = mysqli_connect(
 </div>
 <!-- JAVASCRIPT FILES ========================================= -->
 <script src="js/jquery.min.js"></script><!-- JQUERY MIN JS -->
-<script src="vendor/wow/wow.min.js"></script><!-- WOW JS -->
-<script src="vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script><!-- BOOTSTRAP MIN JS -->
-<script src="vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script><!-- BOOTSTRAP SELECT MIN JS -->
-<script src="vendor/bootstrap-touchspin/bootstrap-touchspin.js"></script><!-- BOOTSTRAP TOUCHSPIN JS -->
+<script src="vendor-js/wow/wow.min.js"></script><!-- WOW JS -->
+<script src="vendor-js/bootstrap/dist/js/bootstrap.bundle.min.js"></script><!-- BOOTSTRAP MIN JS -->
+<script src="vendor-js/bootstrap-select/dist/js/bootstrap-select.min.js"></script><!-- BOOTSTRAP SELECT MIN JS -->
+<script src="vendor-js/bootstrap-touchspin/bootstrap-touchspin.js"></script><!-- BOOTSTRAP TOUCHSPIN JS -->
 
-<script src="vendor/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
-<script src="vendor/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
-<script src="vendor/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
-<script src="vendor/swiper/swiper-bundle.min.js"></script><!-- SWIPER JS -->
-<script src="vendor/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED-->
-<script src="vendor/masonry/masonry-4.2.2.js"></script><!-- MASONRY -->
-<script src="vendor/masonry/isotope.pkgd.min.js"></script><!-- ISOTOPE -->
-<script src="vendor/countdown/jquery.countdown.js"></script><!-- COUNTDOWN FUCTIONS  -->
+<script src="vendor-js/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+<script src="vendor-js/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="vendor-js/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="vendor-js/swiper/swiper-bundle.min.js"></script><!-- SWIPER JS -->
+<script src="vendor-js/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED-->
+<script src="vendor-js/masonry/masonry-4.2.2.js"></script><!-- MASONRY -->
+<script src="vendor-js/masonry/isotope.pkgd.min.js"></script><!-- ISOTOPE -->
+<script src="vendor-js/countdown/jquery.countdown.js"></script><!-- COUNTDOWN FUCTIONS  -->
 <script src="js/dz.carousel.js"></script><!-- DZ CAROUSEL JS -->
-<script src="vendor/lightgallery/dist/lightgallery.min.js"></script>
-<script src="vendor/lightgallery/dist/plugins/thumbnail/lg-thumbnail.min.js"></script>
-<script src="vendor/lightgallery/dist/plugins/zoom/lg-zoom.min.js"></script>
+<script src="vendor-js/lightgallery/dist/lightgallery.min.js"></script>
+<script src="vendor-js/lightgallery/dist/plugins/thumbnail/lg-thumbnail.min.js"></script>
+<script src="vendor-js/lightgallery/dist/plugins/zoom/lg-zoom.min.js"></script>
 <script src="js/dz.ajax.js"></script><!-- AJAX -->
 <script src="js/custom.js"></script><!-- CUSTOM JS -->
 </body>
