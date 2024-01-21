bc26af622260f706344247fb13d241694d88aaf0
Justin Kumpe <jakumpe@kumpes.com>
2024-01-21 16:16:29 -0600

    updated

--- product_labels.php
+++ product_labels.php
@@ -3,63 +3,71 @@ $sku = 'test';
 $title = 'Carbon Fiber PETG Bear Wine Bottle Holder';
 $filament_type = 'OV PLA+';
 $filamnet_color = 'Orange';
-// require_once 'includes/site_params.php';
-// $sku = $_GET['sku'];
-// $conn = mysqli_connect(
-// 	$_ENV['mysql_host'],
-// 	$_ENV['mysql_user'],
-// 	$_ENV['mysql_pass'],
-// 	'Web_3dprints'
-// ) or die("Couldn't connect to server.");
+require_once 'includes/site_params.php';
+$sku = $_GET['sku'];
+$conn = mysqli_connect(
+    $_ENV['mysql_host'],
+    $_ENV['mysql_user'],
+    $_ENV['mysql_pass'],
+    'Web_3dprints'
+) or die("Couldn't connect to server.");
 
-// $product = "
-//   SELECT 
-//     idorders,
-//     first_name,
-//     last_name,
-//     company_name,
-//     email,
-//     street_address,
-//     street_address_2,
-//     city,
-//     state,
-//     zip,
-//     country,
-//     order_date,
-//     payment_method,
-//     notes
-//   FROM
-//     Web_3dprints.orders
-//   WHERE 1=1
-//     AND idorders = $order_id;
-// ";
-// $order_result = mysqli_query($conn, $order_sql);
-// $order = mysqli_fetch_array($order_result, MYSQLI_ASSOC);
+if (strlen($sku) == 15) {
+    $get_color = true;
+} else {
+    $get_color = false;
+}
+$base_sku = substr($sku, 0, 11);
+$dyncolor_sku = $base_sku . '-000';
 
-// $items_sql = "
-//   SELECT 
-//       *
-//   FROM
-//       Web_3dprints.orders__items
-//   WHERE 1=1
-//     AND idorders = $order_id;
-// ";
-// $items_result = mysqli_query($conn, $items_sql);
-// $items = "";
-// while($item = mysqli_fetch_array($items_result)) {
-//   $title = $item["title"];
-//   $sku = $item["sku"];
-//   $qty = $item["qty"];
-//   $item_row = "
-//     <tr>
-//       <td class='sku'>$title</td>
-//       <td class='sku'>$sku</td>
-//       <td align='center'>$qty</td>
-//     </tr>
-//   ";
-//   $items = $items.$item_row;
-// mysqli_close($conn);
-// }
+// Search Product by full SKU
+$product_sql = "
+        SELECT 
+        sku,
+        title
+    FROM
+        Web_3dprints.products
+    WHERE 1=1
+        AND sku = '$sku';
+";
+// Search Product by Dynamic Color
+$product_dyncolor_sql = "
+    SELECT 
+        sku,
+        title
+    FROM
+        Web_3dprints.products
+    WHERE 1=1
+        AND sku = '$dyncolor_sku';
+";
+$product_result = mysqli_query($conn, $product_sql);
+$product_num_results = mysqli_num_rows($product_result);
+$product_data = mysqli_fetch_assoc($product_result);
+
+if ($product_num_results == 0) {
+    $dyncolor_product_result = mysqli_query($conn, $product_dyncolor_sql);
+    $product_data = mysqli_fetch_assoc($dyncolor_product_result);
+}
+if ($get_color) {
+    $color_id = substr($sku, -3);
+    $color_sql = "
+        SELECT 
+            swatch_id,
+            `name`,
+            `type`,
+            color_name
+        FROM
+            Web_3dprints.filament
+        WHERE 1=1
+            AND swatch_id = '$color_id';
+    ";
+    $color_result = mysqli_query($conn, $color_sql);
+    $color_num_results = mysqli_num_rows($color_result);
+    $color_data = mysqli_fetch_assoc($color_result);
+}
+$title = $product_data['title'];
+$filament_type = $color_data['type'];
+$filamnet_color = $color_data['color_name'];
 
 $qr_url = 'https://api.qrserver.com/v1/create-qr-code/?size=50x50&data=' . $sku;
 ?>
