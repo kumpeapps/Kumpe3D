30cf1dbb9e8d4cbc9868f99d6fc953fb0ec2666f
Justin Kumpe <jakumpe@kumpes.com>
2024-03-08 15:46:46 -0600

    -

--- labels/product_labels.php
+++ labels/product_labels.php
@@ -1,13 +1,6 @@
 <?php
-require_once 'dompdf/autoload.inc.php';
-use Dompdf\Dompdf;
-
 $sku = 'test';
 
-$html = file_get_contents('https://www.preprod.kumpe3d.com/product_labels.php?sku=' . $sku);
-$dompdf = new Dompdf();
-$dompdf->loadHtml($html);
-
-$dompdf->render();
-$dompdf->stream();
+$url = 'https://www.preprod.kumpe3d.com/product_labels.php?sku=' . $sku;
+passthru("wkhtmltopdf $url $sku.pdf");
 ?>
