18bc07f62f1bb94e1586be99c83e4d7dd42d358b
Justin Kumpe <jakumpe@kumpes.com>
2024-03-08 16:06:12 -0600

    -

--- labels/product_labels.php
+++ labels/product_labels.php
@@ -5,7 +5,15 @@ $sku = 'test';
 // header("Content-Type: application/octet-stream");
 // header("Content-Disposition: attachment; filename=\"label.pdf\"");
 $url = 'https://www.preprod.kumpe3d.com/product_labels.php?sku=' . $sku;
-$pdf = new Pdf;
+$pdf = new Pdf(array(
+    'no-outline',         // Make Chrome not complain
+    'margin-top'    => 0,
+    'margin-right'  => 0,
+    'margin-bottom' => 0,
+    'margin-left'   => 0,
+    'height'        => "50mm",
+    'width'         => "80mm",
+));
 
 // Add a HTML file, a HTML string or a page from a URL
 $pdf->addPage($url);
