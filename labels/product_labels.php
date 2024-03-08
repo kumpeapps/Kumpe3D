e83eab2166f903e6e091d57ef97031ab2d43071b
Justin Kumpe <jakumpe@kumpes.com>
2024-03-08 15:49:00 -0600

    -

--- labels/product_labels.php
+++ labels/product_labels.php
@@ -1,6 +1,7 @@
 <?php
 $sku = 'test';
-
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=\"label.pdf\"");
 $url = 'https://www.preprod.kumpe3d.com/product_labels.php?sku=' . $sku;
-passthru("wkhtmltopdf $url $sku.pdf");
+passthru("wkhtmltopdf $url $sku.pdf",$result);
 ?>
