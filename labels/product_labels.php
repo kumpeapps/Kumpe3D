b20e7c789d23a6277be0d12c8b137924981740fe
Justin Kumpe <jakumpe@kumpes.com>
2024-03-08 18:11:44 -0600

    -

--- labels/product_labels.php
+++ labels/product_labels.php
@@ -11,7 +11,7 @@ $pdf = new Pdf(array(
     'margin-bottom' => 0,
     'margin-left'   => 0,
     'page-height'   => 40,
-    'page-width'    => 70,
+    'page-width'    => 80,
     'print-media-type',
     'no-outline',
     'disable-smart-shrinking',
