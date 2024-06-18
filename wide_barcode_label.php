426e63022227d66a631547103c676b491e970890
Justin Kumpe <jakumpe@kumpes.com>
2024-06-18 18:14:29 -0500

    -

--- wide_barcode_label.php
+++ wide_barcode_label.php
@@ -108,7 +108,7 @@ $barcode = "https://barcodeapi.org/api/$upc";
             -o-transform: rotate(-90deg);
             float: right;
             max-width: 50mm;
-            margin-top: -10;
+            margin-top: -15;
             margin-right: -5mm;
             font-size: xx-small;
         }
